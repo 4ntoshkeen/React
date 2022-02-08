9b66780d5de0c70d89198d39327c4bfb0315d072
Antoshkeen <g4zefstronof@gmail.com>
2022-02-09 00:41:42 +0300

    Добавьте файлы проекта.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import Board from './Components/Board/Board';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        LET'S PLAY TOGETHER!    
+      </header>
+      <Board/>      
+    </div>
+  );
+}
+
+export default App;
